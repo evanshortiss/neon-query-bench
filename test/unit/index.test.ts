e5dd283cb9ccac96eb336a6080c0c910b0408c07
Evan Shortiss <evanshortiss@gmail.com>
2024-05-21 09:52:06 -0700

    fix: disable next.js caching to produce consistent results on vercel

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -19,7 +19,7 @@ describe('module', () => {
       platform
     } = getBenchmarkInstance(env)
 
-    assert(version === '0.4.1')
+    assert(version === '0.5.2')
     assert(typeof runner === 'function')
     assert(typeof platform.getPlatformName() === 'string')
     assert(typeof platform.getPlatformRegion() === 'string')
