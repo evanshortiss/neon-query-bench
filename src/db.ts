6304b647f65c940f214489ac2e3168d08a50560f
Evan Shortiss <evanshortiss@gmail.com>
2024-06-05 08:49:10 -0700

    fix: missing websocket constructor

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,4 @@
-import { neon, Pool } from "@neondatabase/serverless";
+import { neon, neonConfig, Pool } from "@neondatabase/serverless";
 import { drizzle as drizzleHttp } from "drizzle-orm/neon-http";
 import { drizzle as drizzleWs } from "drizzle-orm/neon-serverless";
 import * as schema from "./schema";
@@ -7,6 +7,7 @@ export type DatabaseWrapper = Awaited<ReturnType<typeof getDatabaseWrapper>>;
 
 export default function getDatabaseWrapper(connectionString: string, mode: "http" | "ws") {
   if (mode === "ws") {
+    neonConfig.webSocketConstructor = require("ws");
     const client = new Pool({ connectionString });
     return drizzleWs(client, { schema, logger: true });
   } else {
